9ef27293a181496b7bc69e80a627c2152075e34d
Vincent Plannthin <vincent@plannthin.net>
2023-11-17 20:54:42 +0100

    validate search terms by pattern

--- src/cdsetool/query.py
+++ src/cdsetool/query.py
@@ -6,6 +6,7 @@ https://documentation.dataspace.copernicus.eu/APIs/OpenSearch.html
 from xml.etree import ElementTree
 from datetime import datetime, date
 import requests
+import re
 
 
 class FeatureQuery:
@@ -65,12 +66,43 @@ def shape_to_wkt(shape):
     )
 
 
+def describe_collection(collection):
+    """
+    Get a list of valid options for a given collection in key value pairs
+    """
+    content = _get_describe_doc(collection)
+    tree = ElementTree.fromstring(content)
+    parameter_node_parent = tree.find(
+        "{http://a9.com/-/spec/opensearch/1.1/}Url[@type='application/json']"
+    )
+
+    parameters = {}
+    for parameter_node in parameter_node_parent:
+        name = parameter_node.attrib.get("name")
+        pattern = parameter_node.attrib.get("pattern")
+        min_inclusive = parameter_node.attrib.get("minInclusive")
+        max_inclusive = parameter_node.attrib.get("maxInclusive")
+        title = parameter_node.attrib.get("title")
+
+        if name:
+            parameters[name] = {
+                    "pattern": pattern,
+                    "minInclusive": min_inclusive,
+                    "maxInclusive": max_inclusive,
+                    "title": title,
+            }
+
+    return parameters
+
+
 def _query_url(collection, search_terms):
-    _validate_search_terms(collection, search_terms)
+    description = describe_collection(collection)
 
     query_list = []
     for key, value in search_terms.items():
-        query_list.append(f"{key}={_serialize_search_term(value)}")
+        val = _serialize_search_term(value)
+        _validate_search_term(key, val, description)
+        query_list.append(f"{key}={val}")
 
     return (
         "https://catalogue.dataspace.copernicus.eu"
@@ -90,36 +122,41 @@ def _serialize_search_term(search_term):
 
     return str(search_term)
 
-
-def _validate_search_terms(collection, search_terms):
-    description = describe_collection(collection)
-    keys = description.keys()
-    for key in search_terms.keys():
-        assert key in keys, (
-            f'search_term with name "{key}" '
-            + f'was not found for collection "{collection}".'
-            + f" Available terms are: {', '.join(keys)}"
-        )
-        # TODO: validate patterns, minInclude, maxInclusive
-
-
-def describe_collection(collection):
-    """
-    Get a list of valid options for a given collection in key value pairs
-    """
-    content = _get_describe_doc(collection)
-    tree = ElementTree.fromstring(content)
-    parameter_node_parent = tree.find(
-        "{http://a9.com/-/spec/opensearch/1.1/}Url[@type='application/json']"
+def _validate_search_term(key, search_term, description):
+    assert key in description.keys(), (
+        f'search_term with name "{key}" '
+        + f'was not found for collection.'
+        + f" Available terms are: {', '.join(description.keys())}"
     )
 
-    parameters = {}
-    for parameter_node in parameter_node_parent:
-        name = parameter_node.attrib.get("name")
-        if name:
-            parameters[name] = True  # TODO: replace with available options
+    _validate_pattern(search_term, description[key]["pattern"])
+    _validate_min_inclusive(search_term, description[key]["minInclusive"])
+    _validate_max_inclusive(search_term, description[key]["maxInclusive"])
 
-    return parameters
+
+def _validate_pattern(search_term, pattern):
+    if not pattern:
+        return
+
+    assert re.match(pattern, search_term), (
+        f"search_term {search_term} does not match pattern {pattern}"
+    )
+
+def _validate_min_inclusive(search_term, min_inclusive):
+    if not min_inclusive:
+        return
+
+    assert search_term >= min_inclusive, (
+        f"search_term {search_term} is less than min_inclusive {min_inclusive}"
+    )
+
+def _validate_max_inclusive(search_term, max_inclusive):
+    if not max_inclusive:
+        return
+
+    assert search_term <= max_inclusive, (
+        f"search_term {search_term} is greater than max_inclusive {max_inclusive}"
+    )
 
 
 _describe_docs = {}
