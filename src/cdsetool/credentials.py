371048c5888a37203d434f7aa8d34d23d8a3d0ce
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-05-13 16:24:28 +0200

    credentials: retry 500/502 response (#120)
    The website will frequently respond
    500/502, so back off and try again when
    that happens.

--- src/cdsetool/credentials.py
+++ src/cdsetool/credentials.py
@@ -68,6 +68,8 @@ class Credentials:  # pylint: disable=too-few-public-methods disable=too-many-in
     and Access Management (IAM) system
     """
 
+    RETRY_CODES = frozenset([413, 429, 500, 502, 503])
+
     def __init__(
         self,
         username=None,
@@ -83,7 +85,7 @@ class Credentials:  # pylint: disable=too-few-public-methods disable=too-many-in
             total=5,
             backoff_factor=0.5,
             raise_on_status=False,
-            status_forcelist=Retry.RETRY_AFTER_STATUS_CODES,
+            status_forcelist=self.RETRY_CODES,
         )
         self.__openid_conf = None
         self.__jwks = None
@@ -134,7 +136,7 @@ class Credentials:  # pylint: disable=too-few-public-methods disable=too-many-in
                 backoff_factor=0.5,
                 allowed_methods=None,
                 raise_on_status=False,
-                status_forcelist=Retry.RETRY_AFTER_STATUS_CODES,
+                status_forcelist=self.RETRY_CODES,
             ),
             proxies=self.__proxies,
         )
