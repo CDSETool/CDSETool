65e867e970f48b1a9276f099da9c7ac385a2bb7e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-10-14 13:53:58 +0200

    logger.py: add type signatures
    Having "Any" as type is not really
    helpful, but this will allow us to
    turn on the pyright switch that
    requires type signatures on everything,
    so new code will have them.

--- src/cdsetool/logger.py
+++ src/cdsetool/logger.py
@@ -4,28 +4,30 @@ Logging utilities.
 This module provides a NoopLogger class that outputs nothing.
 """
 
+from typing import Any
+
 
 class NoopLogger:
     """
     A logger that does nothing.
     """
 
-    def debug(self, msg, *args, **kwargs):
+    def debug(self, msg: object, *args: Any, **kwargs: Any) -> None:
         """
         Log a debug message.
         """
 
-    def error(self, msg, *args, **kwargs):
+    def error(self, msg: object, *args: Any, **kwargs: Any) -> None:
         """
         Log an error message.
         """
 
-    def info(self, msg, *args, **kwargs):
+    def info(self, msg: object, *args: Any, **kwargs: Any) -> None:
         """
         Log an info message.
         """
 
-    def warning(self, msg, *args, **kwargs):
+    def warning(self, msg: object, *args: Any, **kwargs: Any) -> None:
         """
         Log a warning message.
         """
