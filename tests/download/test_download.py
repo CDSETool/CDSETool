14c3c5f323966edbee2732781e8d97672a6bac57
Thibaut Voirand <thibautvoirand@gmail.com>
2025-01-15 20:37:58 +0100

    Centralize tests for S1, S2 and S3 manifests with parameterization

--- tests/download/test_download.py
+++ tests/download/test_download.py
@@ -4,6 +4,7 @@ import logging
 import os
 from unittest import mock
 
+import pytest
 from cdsetool.download import _href_to_url, download_file, download_nodes, filter_files
 
 
@@ -20,36 +21,32 @@ def test_href_to_url():
     assert _href_to_url(odata_url, product_id, product_name, href) == expected_url
 
 
-def test_filter_files_s1():
-    manifest_file_path = "tests/download/mock/sentinel_1/manifest.safe"
-    filtered_files = filter_files(manifest_file_path, "*/calibration-*.xml")
-    assert filtered_files == [
+@pytest.mark.parametrize(
+    "manifest_file_path, pattern, expected_files",
+    [
         (
-            "annotation/calibration/"
-            "calibration-s1a-iw-grd-vh-20241217t061735-20241217t061800-057028-07020f-002.xml"
+            "tests/download/mock/sentinel_1/manifest.safe",
+            "*/calibration-*.xml",
+            [
+                "annotation/calibration/calibration-s1a-iw-grd-vh-20241217t061735-20241217t061800-057028-07020f-002.xml",
+                "annotation/calibration/calibration-s1a-iw-grd-vv-20241217t061735-20241217t061800-057028-07020f-001.xml",
+            ],
         ),
         (
-            "annotation/calibration/"
-            "calibration-s1a-iw-grd-vv-20241217t061735-20241217t061800-057028-07020f-001.xml"
+            "tests/download/mock/sentinel_2/manifest.safe",
+            "*TCI.jp2",
+            ["GRANULE/L1C_T17UPV_A040535_20241209T162603/IMG_DATA/T17UPV_20241209T162609_TCI.jp2"],
         ),
-    ]
-
-
-def test_filter_files_s2():
-    manifest_file_path = "tests/download/mock/sentinel_2/manifest.safe"
-    filtered_files = filter_files(manifest_file_path, "*TCI.jp2")
-    assert filtered_files == [
         (
-            "GRANULE/L1C_T17UPV_A040535_20241209T162603/"
-            "IMG_DATA/T17UPV_20241209T162609_TCI.jp2"
-        )
-    ]
-
-
-def test_filter_files_s3():
-    manifest_file_path = "tests/download/mock/sentinel_3/manifest.xml"
-    filtered_files = filter_files(manifest_file_path, "*Oa02_reflectance.nc")
-    assert filtered_files == [("Oa02_reflectance.nc")]
+            "tests/download/mock/sentinel_3/manifest.xml",
+            "*Oa02_reflectance.nc",
+            [("Oa02_reflectance.nc")],
+        ),
+    ],
+)
+def test_filter_files(manifest_file_path, pattern, expected_files):
+    filtered_files = filter_files(manifest_file_path, pattern)
+    assert filtered_files == expected_files
 
 
 def test_filter_files_with_exclude():
